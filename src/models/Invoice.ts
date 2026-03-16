e961715b5f3627965aa48370c57d48ad8bd1d609
Balthild <ibalthild@gmail.com>
2026-03-16 01:11:54 -0500

    refactor: format code

--- src/models/Invoice.ts
+++ src/models/Invoice.ts
@@ -1,32 +1,15 @@
 export class Invoice {
-  invoice_id: number
-  user_id: number
-  account_id: number
-  vendor_id: number
-  total_amount: number
-  voucher_date: Date
-  created_date: Date
-
   constructor(
-    invoice_id: number,
-    user_id: number,
-    account_id: number,
-    vendor_id: number,
-    total_amount: number,
-    voucher_date: Date,
-    created_date: Date
-  ) 
-  {
-    this.invoice_id = invoice_id
-    this.user_id = user_id
-    this.account_id = account_id
-    this.vendor_id = vendor_id
-    this.total_amount = total_amount
-    this.voucher_date = voucher_date
-    this.created_date = created_date
-  }
+    public invoice_id: number,
+    public user_id: number,
+    public account_id: number,
+    public vendor_id: number,
+    public total_amount: number,
+    public voucher_date: Date,
+    public created_date: Date,
+  ) {}
 
   getTotal(): number {
-    return this.total_amount
+    return this.total_amount;
   }
 }
