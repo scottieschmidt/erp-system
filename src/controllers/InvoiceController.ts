b25a005e5952334ea5b5d6aa04abf0780d1b3367
Balthild <ibalthild@gmail.com>
2026-03-16 01:11:54 -0500

    refactor: format code

--- src/controllers/InvoiceController.ts
+++ src/controllers/InvoiceController.ts
@@ -1,13 +1,13 @@
-import { Invoice } from "../models/Invoice"
+import { Invoice } from "../models/Invoice";
 
 export class InvoiceController {
   createInvoice(invoice: Invoice): boolean {
-    console.log("Creating invoice:", invoice)
-    return true
+    console.log("Creating invoice:", invoice);
+    return true;
   }
 
   getInvoice(id: number): Invoice | null {
-    console.log(id)
-    return null
+    console.log(id);
+    return null;
   }
 }
